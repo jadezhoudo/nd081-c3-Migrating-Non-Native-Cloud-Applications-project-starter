f6a81c2236d2a4a0da03b4cf69fc4ffb7dd9c8f4
ChauDN1 <mr.do036@gmail.com>
2023-11-18 11:32:55 +0700

    first commit

--- web/config.py
+++ web/config.py
@@ -2,20 +2,23 @@ import os
 
 app_dir = os.path.abspath(os.path.dirname(__file__))
 
+
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
-    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    POSTGRES_URL = "codeninja.postgres.database.azure.com"  # TODO: Update value
+    POSTGRES_USER = "codeninja123"  # TODO: Update value
+    POSTGRES_PW = "CanTho@123"  # TODO: Update value
+    POSTGRES_DB = "techconfdb"  # TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(
+        user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
-    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    SERVICE_BUS_CONNECTION_STRING = 'Endpoint=sb://notificationqueuecodeninja.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=P9CPBWvBayHKQXCNMyv+F5AjpCF5pS7C9+ASbEuJJB4='  # TODO: Update value
+    SERVICE_BUS_QUEUE_NAME = 'notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
-    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+    SENDGRID_API_KEY = ''  # Configuration not required, required SendGrid Account
+
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
